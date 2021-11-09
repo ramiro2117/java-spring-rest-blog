819c461d05a705f0b2933281a8a24af48e921a17
ramiro2117 <91563981+ramiro2117@users.noreply.github.com>
2021-11-09 14:33:04 -0600

    - added a posts arraylist in Author.java, the getter and setter, and initialized in constructor - updated AuthorRepository.java to extend JpaRepository for basic CRUD functions - added an authors arraylist and and authorrepository, set author for post, and added post to author. - in Post.java we added an Author variable and getter and setter - in PostRepository.java we added a findByAuthor_Lastname method

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -14,6 +14,7 @@ import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
+    private final AuthorRepository authorRepository;
     private final PostRepository postRepository;
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On The Go", "The %s - Your New Favorite Accessory"};
@@ -23,20 +24,33 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Author> authors = new ArrayList<>();
 
     @Autowired
-    public DatabaseLoader(PostRepository postRepository) {
+    public DatabaseLoader(PostRepository postRepository, AuthorRepository authorRepository) {
         this.postRepository = postRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        authors.addAll(Arrays.asList(
+                new Author("sholderness", "Sarah",  "Holderness", "password"),
+                new Author("tbell", "Tom",  "Bell", "password"),
+                new Author("efisher", "Eric",  "Fisher", "password"),
+                new Author("csouza", "Carlos",  "Souza", "password")
+        ));
+        authorRepository.saveAll(authors);
+
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
+            Author author = authors.get(i % authors.size());
 
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
+            post.setAuthor(author);
+            author.addPost(post);
             randomPosts.add(post);
         });
         postRepository.saveAll(randomPosts);
+        authorRepository.saveAll(authors);
     }
 }
